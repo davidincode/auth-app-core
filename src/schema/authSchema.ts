cb744f1ad4ecc0b5b567d19f77469616ab286c0e
David <jefferson-education@outlook.com>
2023-08-28 14:12:25 -0500

    Adding types

--- src/schema/authSchema.ts
+++ src/schema/authSchema.ts
@@ -1,12 +1,22 @@
 import { z } from 'zod'
 
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
 export const signupSchema = z.object({
   name: z.string().min(2).max(50),
-  email: z.string().email(),
+  email: z.string().email().refine(async (email) => {
+    const existingUser = await prisma.user.findUnique({ where: { email } })
+    return existingUser === null
+  }, { message: 'Email already exists' }),
   password: z.string().min(6)
 })
+
+export type SignupData = z.infer<typeof signupSchema>
 
 export const signinSchema = z.object({
   email: z.string().email(),
   password: z.string().min(6)
 })
+
+export type SigninData = z.infer<typeof signinSchema>
