fa807275147420b48497d2f3e5ca9745685543e8
David <jefferson-education@outlook.com>
2023-08-28 17:25:46 -0500

    Schema correction

--- src/schema/authSchema.ts
+++ src/schema/authSchema.ts
@@ -1,14 +1,8 @@
 import { z } from 'zod'
 
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
-
 export const signupSchema = z.object({
   name: z.string().min(2).max(50),
-  email: z.string().email().refine(async (email) => {
-    const existingUser = await prisma.user.findUnique({ where: { email } })
-    return existingUser === null
-  }, { message: 'Email already exists' }),
+  email: z.string().email(),
   password: z.string().min(6)
 })
 
